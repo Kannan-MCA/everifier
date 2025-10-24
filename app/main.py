ff7747dac233a2e2fa215c55feda512e0928905d
MCET-CPG-JAVA <mcetcgp@gmail.com>
2025-10-24 01:10:50 +0000

    py commit

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@
 from fastapi import FastAPI, HTTPException
 from app.api.routes import router
 from app.utils.logger import setup_logging
-
 app = FastAPI(title="Email Validator API")
 app.include_router(router)
 setup_logging(app)
